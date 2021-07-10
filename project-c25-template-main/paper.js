e184fd4917a80fba07b824aa3bbeeb7d2d62cd91
vidyarnandanan <70987975+vidyarnandanan@users.noreply.github.com>
2021-07-10 20:46:46 +0530

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,27 @@
+class paper{
+    constructor(x,y,radius) {
+        var options = {
+            'restitution':0.1,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x,y,radius, options);
+       
+        this.image=loadImage("paper.png")
+        this.radius = radius;
+        World.add(world, this.body);
+      }
+      display(){
+        
+          var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image( this.image,0, 0,this.radius*2);
+        pop(); 
+        
+        
+        
+      }
+}
